a8fcd6e4677a867254e14fbc55a8ee85fb89281f
hyobum <hyob1203@naver.com>
2019-04-14 00:25:28 +0900

    add local define

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import FastScroll from './FastScroll.vue'
 
+const fastScroll = FastScroll
 const install = function (Vue, globalOptions) {
   Vue.component(FastScroll.name, FastScroll)
 }
-const VueFastScroll = { install }
+const VueFastScroll = { install, fastScroll }
 
 export default VueFastScroll
-export { install }
+export { install, fastScroll }
