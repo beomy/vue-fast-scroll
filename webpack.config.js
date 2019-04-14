b2871df4888182c1e23bff6526442efadae63a31
hyobum <hyob1203@naver.com>
2019-04-14 23:12:29 +0900

    production mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist') + '/js',
